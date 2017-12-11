68519290a3d72da17021aabe6a5bb87d736d8aae
Nick <mykola.kb@gmail.com>
2017-12-11 04:35:27 +0200

    Revert "Merge remote-tracking branch 'origin/andrij' into mykola"
    This reverts commit 3672a90ee4e2e37185eb009e1960797e33cc6c47.

--- app/src/main/java/sadman/volme/BonusesActivity.java
+++ app/src/main/java/sadman/volme/BonusesActivity.java
@@ -1,54 +1,14 @@
 package sadman.volme;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.ListView;
-
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * Created by sadman on 03/12/17.
- */
 
 public class BonusesActivity extends AppCompatActivity {
 
-    private FirebaseDatabase mFirebaseDatabase;
-    private DatabaseReference mEventsDatabaseReference;
-    private ChildEventListener mChildEventListener;
-
-    private EventsAdapter mEventCardsAdapter;
-    private ListView mEventListView;
 
 
-    public void openBonuses(View v) {
-        Intent i = new Intent(this, BonusesActivity.class);
-        startActivity(i);
-    }
-
-    public void openHome(View v) {
-        Intent i = new Intent(this, home.class);
-        startActivity(i);
-    }
-
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bonuses);
-
-
-
-
-    }
 }
--- app/src/main/java/sadman/volme/Event.java
+++ app/src/main/java/sadman/volme/Event.java
@@ -1,18 +1,5 @@
 package sadman.volme;
 
-import android.content.Intent;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.ListView;
-
-
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by sadman on 05/12/17.
  */
@@ -25,73 +12,34 @@ public class Event {
     private String event_location;
     private String event_tag;
 
-    public Event() {
+    public Event(){
     }
 
     public Event(String organization_name, String event_title, String event_description, String event_data, String event_location, String event_tag) {
         this.organization_name = organization_name;
-        this.event_title = event_title;
+        this.event_title =event_title;
         this.event_description = event_description;
         this.event_data = event_data;
         this.event_location = event_location;
         this.event_tag = event_tag;
     }
 
-    public String getOrganization_name() {
-        return organization_name;
-    }
+    public String getOrganization_name() {return organization_name;}
+    public void setOrganization_name(String organization_name) {this.organization_name = organization_name;}
 
-    public void setOrganization_name(String organization_name) {
-        this.organization_name = organization_name;
-    }
+    public String getEvent_title() {return event_title;}
+    public void setEvent_title(String event_title) {this.event_title = event_title;}
 
-    public String getEvent_title() {
-        return event_title;
-    }
+    public String getEvent_description() {return event_description;}
+    public void setEvent_description(String event_description) {this.event_description = event_description;}
 
     public String getEvent_data() {return event_data;}
     public void setEvent_data(String event_data) {this.event_data = event_data;}
 
-    public String getEvent_description() {
-        return event_description;
-    }
+    public String getEvent_location() {return event_location;}
+    public void setEvent_location(String event_location) {this.event_location = event_location;}
 
-    public void setEvent_description(String event_description) {
-        this.event_description = event_description;
-    }
+    public String getEvent_tag() {return event_tag;}
+    public void setEvent_tag(String event_tag) {this.event_tag = event_tag;}
 
-    public String getEvent_data() {
-        return event_date;
-    }
-
-    public void setEvent_date(String event_date) {
-        this.event_date = event_date;
-    }
-
-    public String getEvent_location() {
-        return event_location;
-    }
-
-    public void setEvent_location(String event_location) {
-        this.event_location = event_location;
-    }
-
-    public String getEvent_tag() {
-        return event_tag;
-    }
-
-    public void setEvent_tag(String event_tag) {
-        this.event_tag = event_tag;
-    }
-
-
-
-    public class event extends AppCompatActivity {
-        public void openEvent(View v) {
-            Intent i = new Intent(this, EventActivity.class);
-            startActivity(i);
-
-
-        }
-    }
 }
